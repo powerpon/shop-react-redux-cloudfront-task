f5e6a5336188b7149d7d599d9e7b53da280d0bfd
Stefan Novcic <stefan_novcic@epam.com>
2024-02-22 18:25:05 +0100

    adapted api URLs to invoke data hosting lambdas

--- src/constants/apiPaths.ts
+++ src/constants/apiPaths.ts
@@ -1,9 +1,9 @@
 const API_PATHS = {
-  product: "https://.execute-api.eu-west-1.amazonaws.com/dev",
-  order: "https://.execute-api.eu-west-1.amazonaws.com/dev",
-  import: "https://.execute-api.eu-west-1.amazonaws.com/dev",
-  bff: "https://.execute-api.eu-west-1.amazonaws.com/dev",
-  cart: "https://.execute-api.eu-west-1.amazonaws.com/dev",
+  product: "https://bdyzlvccbc.execute-api.us-east-1.amazonaws.com",
+  order: "https://bdyzlvccbc.execute-api.us-east-1.amazonaws.com",
+  import: "https://bdyzlvccbc.execute-api.us-east-1.amazonaws.com",
+  bff: "https://bdyzlvccbc.execute-api.us-east-1.amazonaws.com",
+  cart: "https://bdyzlvccbc.execute-api.us-east-1.amazonaws.com",
 };
 
 export default API_PATHS;
