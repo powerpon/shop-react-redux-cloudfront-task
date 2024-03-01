9c7c22a762a40ff980096e6e398ef19aff831734
Stefan Novcic <stefan_novcic@epam.com>
2024-03-01 12:31:04 +0100

    api path change

--- src/constants/apiPaths.ts
+++ src/constants/apiPaths.ts
@@ -1,9 +1,9 @@
 const API_PATHS = {
-  product: "https://bdyzlvccbc.execute-api.us-east-1.amazonaws.com",
-  order: "https://bdyzlvccbc.execute-api.us-east-1.amazonaws.com",
-  import: "https://bdyzlvccbc.execute-api.us-east-1.amazonaws.com",
-  bff: "https://bdyzlvccbc.execute-api.us-east-1.amazonaws.com",
-  cart: "https://bdyzlvccbc.execute-api.us-east-1.amazonaws.com",
+  product: "https://7nrsbnkzji.execute-api.us-east-1.amazonaws.com",
+  order: "https://7nrsbnkzji.execute-api.us-east-1.amazonaws.com",
+  import: "https://7nrsbnkzji.execute-api.us-east-1.amazonaws.com",
+  bff: "https://7nrsbnkzji.execute-api.us-east-1.amazonaws.com",
+  cart: "https://7nrsbnkzji.execute-api.us-east-1.amazonaws.com",
 };
 
 export default API_PATHS;
