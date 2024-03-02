0579578955f73c521b387153775b8d5b51132a05
Stefan Novcic <stefan_novcic@epam.com>
2024-03-02 20:59:58 +0100

    added new lambda url to import api path

--- src/constants/apiPaths.ts
+++ src/constants/apiPaths.ts
@@ -1,7 +1,7 @@
 const API_PATHS = {
   product: "https://7nrsbnkzji.execute-api.us-east-1.amazonaws.com",
   order: "https://7nrsbnkzji.execute-api.us-east-1.amazonaws.com",
-  import: "https://yh3cfk9oik.execute-api.us-east-1.amazonaws.com",
+  import: "https://0oy4migzkb.execute-api.us-east-1.amazonaws.com",
   bff: "https://7nrsbnkzji.execute-api.us-east-1.amazonaws.com",
   cart: "https://7nrsbnkzji.execute-api.us-east-1.amazonaws.com",
 };
